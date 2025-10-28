c83706decf1f524f644fb1e937278e0456dd03a0
Cesar Carmona <clcg852@gmail.com>
2025-10-27 23:34:35 -0600

    feat: agregar previsualización interactiva del PDF con pdf-lib y conexión con formulario contable

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,9 @@
 import logo from "../assets/logo.png";
-//import '../styles/navbar.css'
 
 export function Navbar() { 
   return (
     <nav>
-      <a href="/" className="logo">
+      <a href="https://github.com/cesarleroy/bacon" className="logo">
         <img src={logo} alt="bacon-logo" height="36px"/>
       </a>
     </nav>
